d9fab8232984680d6b02efe531396460729ac351
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-04-08 17:27:51 +0200

    Inserimento dei numeri in pagina (nei box).

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,4 +8,9 @@ const limit = 100;
 
 for(let i=1; i<=limit; i++){
   console.log(i);
+  const boxEl = document.createElement("div");
+  // Gli scrivo la classe
+  boxEl.className = "box";
+  boxEl.append(i);
+  container.append(boxEl);
 }
