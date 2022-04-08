e2a470f1140ebc0a5c9edacdedcd998ee88b6c71
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-04-08 17:16:53 +0200

    Stampa in console dei numeri da 1 a 100.

---
+++ assets/JS/script.js
@@ -0,0 +1,11 @@
+/*
+Stampare in console i numeri da 1 a 100.
+ */
+
+const container = document.querySelector(".container");
+
+const limit = 100;
+
+for(let i=1; i<=limit; i++){
+  console.log(i);
+}
